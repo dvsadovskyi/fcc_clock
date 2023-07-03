9899cfdf92547d0e63912e714b2e7a8cb619efad
dvsadovskyi_LOCAL_WORK <dvsadovskyi>
2023-07-03 11:14:10 +0300

    some styly

--- index.js
+++ index.js
@@ -7,27 +7,32 @@ function App() {
                     <div className="break-section">
                         <div id="break-label">Break Length</div>
                         <div className="break-adj">
-                            <i id="break-decrement">down</i>
-                            <div id="break-length">break time</div>
-                            <i id="break-increment">up</i>
+                            <div id="break-decrement"><i className="fas fa-arrow-alt-circle-down fa-lg"></i></div>
+                            <div id="break-length">5</div>
+                            <div id="break-increment"><i className="fas fa-arrow-alt-circle-up fa-lg"></i></div>
                         </div>
                     </div>
                     <div className="session-section">
                         <div id="session-label">Session Length</div>
                         <div className="session-adj">
-                            <i id="session-decrement">down</i>
-                            <div id="session-length">session time</div>
-                            <i id="session-increment">up</i>
+                            <div id="session-decrement"><i className="fas fa-arrow-alt-circle-down fa-lg"></i></div>
+                            <div id="session-length">25</div>
+                            <div id="session-increment"><i className="fas fa-arrow-alt-circle-up fa-lg"></i></div>
                         </div>
                     </div>
                 </div>
                 <div className="timer">
                     <div id="timer-label">Session</div>
-                    <div id="time-left">time</div>
+                    <div id="time-left">60</div>
                 </div>
                 <div className="start-reset">
-                    <i id="start_stop">start-pause</i>
-                    <i id="reset">reset</i>
+                    <div id="start_stop">
+                        <i className="fas fa-play fa-lg"></i>
+                        <i className="fas fa-pause fa-lg"></i>
+                    </div>
+                    <div id="reset">
+                        <i className="fas fa-redo fa-lg"></i>
+                    </div>
                 </div>
                 <footer>
                     <p id="footer-title">Designed and Coded by</p>
